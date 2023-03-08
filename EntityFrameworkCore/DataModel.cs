a5721c616ab26345f14c8e94832aadc6e2491648
sleepygloa <sleepygloa@gmail.com>
2023-03-09 08:03:12 +0900

    init

---
+++ EntityFrameworkCore/DataModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace EntityFrameworkCore
+{
+	public class Item
+	{
+		//이름Id -> PK
+		public int ItemId { get; set; }
+		public int TmeplateId { get; set; }
+		public DateTime CreateDate { get; set; }
+
+		//다른 클래스 참조 -> FK (Navigational Property)
+		public int OwnerId { get; set; }
+		public Player Owner { get; set; }
+	}
+
+	//클래스이름 = 테이블 이름 = Player
+	public class Player
+	{
+		//이름id -> PK
+		public int PlayerId { get; set; }
+		public string name { get; set; }
+
+	}
+}
+
